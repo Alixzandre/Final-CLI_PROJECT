593cbac0f225c864eef55a93f6f96c7e3d88778b
Alixzandre <54338869+Alixzandre@users.noreply.github.com>
2020-02-16 21:10:43 -0600

    Add files via upload
    Fixed reformating error

---
+++ electronic_devices/lib/electronic_devices.rb
@@ -0,0 +1,7 @@
+
+require "nokogiri"
+require "pry"
+require "open-uri"
+require_relative "./electronic_devices/version"
+require_relative './electronic_devices/cli'
+require_relative './electronic_devices/menus'
---
+++ electronic_devices/lib/electronic_devices/version.rb
@@ -0,0 +1,3 @@
+module ElectronicDevices
+  VERSION = "0.1.0"
+end
